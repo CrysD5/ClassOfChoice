06c0362eeaa56304e76e7aa333e475b63d6d37ec
Crys David <matrim.ivy@gmail.com>
2021-09-27 22:29:31 -0400

    set up my code skeleton, will flesh out during the week

---
+++ Bookshelf.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Choose_Your_Class
+{
+    public class Bookshelf
+    {
+        public List<string> Shelf = new List<string>();
+
+        /*
+         public void InteractWithAll()
+        {
+            Console.Clear();
+            bool keepThinking = true;
+            while (keepThinking == true)
+            {
+                Console.WriteLine("Choose an action!");
+                Console.WriteLine("1. Feed all pets.");
+                Console.WriteLine("2. Play with all pets.");
+                Console.WriteLine("3. Heal all pets.");
+                Console.WriteLine("Press Q to return to main menu.");
+                string UserInput = Console.ReadLine().ToLower();
+
+                int index = 1;
+
+                switch (UserInput)
+                {
+                    case "1":
+                        foreach (Pet pet1 in ListOfPets)
+                        {
+                            Console.WriteLine($"{index}: {pet1.GetName()}, {pet1.GetSpecies()}");
+                            pet1.Feed();
+                            index++;
+                        }
+                        break;
+                    case "2":
+                        foreach (Pet pet1 in ListOfPets)
+                        {
+                            Console.WriteLine($"{index}: {pet1.GetName()}, {pet1.GetSpecies()}");
+                            pet1.Play();
+                            index++;
+                        }
+                        break;
+                    case "3":
+                        foreach (Pet pet1 in ListOfPets)
+                        {
+                            Console.WriteLine($"{index}: {pet1.GetName()}, {pet1.GetSpecies()}");
+                            pet1.SeeDoctor();
+                            index++;
+                        }
+                        break;
+                    case "q":
+                        keepThinking = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            Console.Read();
+           
+         */
+
+
+    }
+}
+
+
+
+        /*public void AddBook(Bookshelf book)
+        {
+            Bookshelf.Add(book);
+        }*/
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Choose_Your_Class
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Welcome to your bookshelf!");
+
+            bool keepReading = true;
+
+            while (keepReading)
+            {
+                Console.WriteLine("What would you like to do today?");
+
+                Console.WriteLine("1. Add new books to read.");
+                Console.WriteLine("2. Add a book to future reading.");
+                Console.WriteLine("3. Add a book to favorites.");
+                Console.WriteLine("4. Move a book to finished pile.");
+                Console.WriteLine("Q to exit");
+
+                string userInput = Console.ReadLine().ToLower();
+
+                switch (userInput)
+                {
+                    case "1":
+                        //Add book to read
+                        break;
+                    case "2":
+                        //Add to future reads
+                        break;
+                    case "3":
+                        //Add to favs
+                        break;
+                    case "4":
+                        //Move book to finished stack
+                        break;
+                    case "q":
+                        keepReading = false;
+                        break;
+
+
+                    default:
+
+
+                        break;
+                }
+
+            }
         }
+        /*static void Main(string[] args)
+          {
+            Console.WriteLine("Welcome to your library!");
+          }*/
     }
 }
